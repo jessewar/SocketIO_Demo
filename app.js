13c69376c9c93bad88f139e5824f39d2047e61e1
Jesse Warren <jesse@jesse.warren>
2014-02-07 16:28:51 -0800

    added usernames

--- app.js
+++ app.js
@@ -3,12 +3,25 @@ var app = express();
 var server = app.listen(3000);
 var io = require('socket.io').listen(server);
 
+var nameList = [];
+
 app.get('/', function(request, response) {
 	response.sendfile(__dirname + '/index.html');
 });
 
 io.sockets.on('connection', function(socket) {
+	socket.on('name sent', function(data) {
+		socket.name = data;  // add the name to this particular socket
+		nameList.push(socket.name);
+		updateNames();
+	});
+
 	socket.on('message sent', function(data) {
 		io.sockets.emit('new message', data);
 	});
+
+	// emits the updated name list to all sockets
+	function updateNames() {
+		io.sockets.emit('update names', nameList)
+	}
 });
